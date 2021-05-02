17e1515d7cd9ba973001739846ddf5cca73346e8
Sumit Sudalkar <69747683+sumitsudalkar@users.noreply.github.com>
2021-05-02 20:20:57 +0530

    Update emailval.py

--- week2task5/emailval.py
+++ week2task5/emailval.py
@@ -1,16 +1,8 @@
-import re
-
-n=input()
-for i in range(n):
-    line=raw_input()
-    name,email=line.split(' ')
-    pattern="<[a-z][a-zA-Z0-9\-\.\_]+@[a-zA-Z]+\.[a-zA-Z]{1,3}>"
-    if bool(re.match(pattern,email)):
-        print name,email
-
-# Enter your code here. Read input from STDIN. Print output to STDOUT
-from re import match, compile
-
-pattern = compile('^[-+]?\d*\.\d+$')
-for _ in range(int(input())):
-    print(bool(pattern.match(input())))
+import re, email.utils
+n = int(input())
+for t in range(n):
+    s = input()
+    parsed_email = email.utils.parseaddr(s)[1].strip()
+    match_result = bool(re.match(r'(^[A-Za-z][A-Za-z0-9\._-]+)@([A-Za-z]+)\.([A-Za-z]{1,3})$',parsed_email))
+    if match_result == True:
+        print(s)
