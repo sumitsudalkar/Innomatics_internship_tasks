d755a81b11229e3ace4d8a6102833d734c4766ed
Sumit Sudalkar <69747683+sumitsudalkar@users.noreply.github.com>
2021-05-02 17:25:57 +0530

    Create int_pow.py

---
+++ week2task3/int_pow.py
@@ -0,0 +1,9 @@
+a=int(input())
+if(a>=1 and a<=1000):
+    b=int(input())
+    if(b>=1 and b<=1000):
+        c= int(input())
+        if(c>=1 and c<=1000):
+            d=int(input())
+result = pow(a,b)+pow(c,d)
+print(result)
